7933a94f4a22982642299b442d932f0751f73959
AndrewPTS <acastelino@wisc.edu>
2020-10-08 15:53:06 -0500

    cmake builds but without libs

---
+++ build/build_win/CMakeFiles/WizartGaem.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Scripts 'n' Stuff/Wizard Game/Wizart-Gaem/Engine/Engine.cpp" "E:/Scripts 'n' Stuff/Wizard Game/Wizart-Gaem/build/build_win/CMakeFiles/WizartGaem.dir/E_/Scripts_'n'_Stuff/Wizard_Game/Wizart-Gaem/Engine/Engine.cpp.obj"
+  "E:/Scripts 'n' Stuff/Wizard Game/Wizart-Gaem/Engine/Screen.cpp" "E:/Scripts 'n' Stuff/Wizard Game/Wizart-Gaem/build/build_win/CMakeFiles/WizartGaem.dir/E_/Scripts_'n'_Stuff/Wizard_Game/Wizart-Gaem/Engine/Screen.cpp.obj"
+  "E:/Scripts 'n' Stuff/Wizard Game/Wizart-Gaem/Rendering/Sprite.cpp" "E:/Scripts 'n' Stuff/Wizard Game/Wizart-Gaem/build/build_win/CMakeFiles/WizartGaem.dir/E_/Scripts_'n'_Stuff/Wizard_Game/Wizart-Gaem/Rendering/Sprite.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
