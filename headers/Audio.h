80d7c1622478c6ee1c986b5baeec6c138b68e5a3
Pascal King <pmkgbwis@gmail.com>
2020-10-14 02:19:33 -0500

    Audio should play once it's imported

--- headers/Audio.h
+++ headers/Audio.h
@@ -20,6 +20,7 @@ public:
     }
     void update();
     void ping();
+    virtual int play(int loops) {return -1;}
 };
 
 struct Mus : public Mix {
@@ -29,6 +30,7 @@ protected:
 public:
     Mus(const char* path) : Mix(path) {}
     Mix_Music* data = nullptr;
+    int play(int loops);
 };
 
 struct Sfx : public Mix{
@@ -38,16 +40,26 @@ protected:
 public:
     Sfx(const char* path) : Mix(path) {}
     Mix_Chunk* data = nullptr;
+    int play(int loops);
 };
 
 struct Sound {
 private:
     Mix sound;
+    int channel;
 public:
     Sound(const char* name);
+
+    int setVolume(int vol);
+
+    void play(int loops);
+
+    void stop();
 };
 
 class AudioSource {
+
+    int volume;
 
     Sound* play(const char* str);
 
--- src/audio/Audio.cpp
+++ src/audio/Audio.cpp
@@ -6,6 +6,11 @@
 
 std::unordered_map<const char*, Mix*> soundMap;
 
+Mix getMix(const char* name) {
+    Mix* ptr = soundMap[name];
+    return *ptr;
+}
+
 void imp::importAudio(const char* path) {
     imp_i::SoundData data = imp_i::parseSound(path);
     const char* name = data.name;
@@ -26,12 +31,21 @@ void Mus::unload() {
     Mix_FreeMusic(data);
 }
 
+int Mus::play(int loops) {
+    Mix_PlayMusic(data, loops);
+    return -2;
+}
+
 void Sfx::lazyload() {
     data = Mix_LoadWAV(path);
 }
 
 void Sfx::unload() {
     Mix_FreeChunk(data);
+}
+
+int Sfx::play(int loops) {
+    return Mix_PlayChannel(-1, data, loops);
 }
 
 void Mix::update() {
@@ -50,21 +64,53 @@ void Mix::ping() {
 }
 
 Sound* AudioSource::play(const char* name) {
-    return nullptr;
+    Sound* sound = new Sound(name);
+    sound->play(0);
+    sound->setVolume(volume);
+    return sound;
 }
 
 Sound* AudioSource::play(const char* name, AudioContext ctx) {
-    return nullptr;
+        Sound* sound = new Sound(name);
+    sound->play(0);
+    sound->setVolume(volume);
+    return sound;
 }
 
 Sound* AudioSource::loop(const char* name, int loops) {
-    return nullptr;
+    Sound* sound = new Sound(name);
+    sound->play(loops);
+    sound->setVolume(volume);
+    return sound;
 }
 
 Sound* AudioSource::loop(const char* name, int loops, AudioContext ctx) {
-    return nullptr;
+    Sound* sound = new Sound(name);
+    sound->play(loops);
+    sound->setVolume(volume);
+    return sound;
 }
 
 void sfx_i::clean() {
 
+}
+
+Sound::Sound(const char* name) : sound(getMix(name)) {}
+
+void Sound::play(int loops) {
+    channel = sound.play(loops);
+}
+
+int Sound::setVolume(int vol) {
+    if (channel == -2)
+        return Mix_VolumeMusic(vol);
+    else
+        return Mix_Volume(channel, vol);
+}
+
+void Sound::stop() {
+    if (channel == -2)
+        Mix_HaltMusic();
+    else
+        Mix_HaltChannel(channel);
 }
