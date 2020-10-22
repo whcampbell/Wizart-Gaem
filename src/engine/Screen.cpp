82e12abab1c7e50692b9a2e53c3fe7537397c780
Pascal King <pmkgbwis@gmail.com>
2020-10-22 15:11:24 -0500

    FIX MAC :)

--- src/engine/Screen.cpp
+++ src/engine/Screen.cpp
@@ -6,7 +6,6 @@ static const int WIDTH = 1200;
 static const int HEIGHT = 675;
 
 static SDL_Window* window = NULL;
-static SDL_Surface* surface = NULL;
 static SDL_Renderer* renderer = NULL;
 
 bool initWindow() {
@@ -19,12 +18,7 @@ bool initWindow() {
         if (window == NULL) {
             std::cout << "Window creation failed. Error: \n" << SDL_GetError() << std::endl;
         } else {
-            surface = SDL_GetWindowSurface(window);
-//            if (surface == NULL) {
-//                std::cout << "Failed to create surface" << SDL_GetError() << std::endl;
-//                return false;
-//            }
-            renderer = SDL_CreateRenderer(window, -1, 0);
+            renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
             if (renderer == NULL) {
                 std::cout << "Failed to create renderer" << SDL_GetError() << std::endl;
                 return false;
@@ -38,9 +32,6 @@ bool initWindow() {
 void closeWindow() {
 	SDL_DestroyRenderer(renderer);
     renderer = NULL;
-    
-//    SDL_FreeSurface(surface);
-//	surface = NULL;
 
 	SDL_DestroyWindow(window);
 	window = NULL;
