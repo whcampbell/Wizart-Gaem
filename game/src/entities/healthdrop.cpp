f44b61b0114ed9a806ffd37fe9973a64d77cd545
Will <Campbell>
2020-11-08 17:20:13 -0600

    added health drop  - created health drop entity  - added one to test scene   - added pragma to mana item

---
+++ game/src/entities/healthdrop.cpp
@@ -0,0 +1,38 @@
+#include "entities/healthdrop.h"
+#include "components/hitpoints.h"
+#include "camera.h"
+#include "entitymanager.h"
+
+HealthDrop::HealthDrop() {
+    activeSprite = new Sprite("life_drop");
+    *align->x_internal = 16;
+    *align->y_internal = 16;
+    Hitbox* hit = registerHitbox("hitbox");
+    hit->align = align;
+    hit->w = 16;
+    hit->h = 16;
+}
+
+void HealthDrop:: update() {
+    for (auto iterator : *entities:: all()) {
+        if (hitbox::collision(iterator->hitbox("pickupbox"), hitbox("hitbox"))) {
+            if (iterator->has<Hitpoints>()) {
+
+                Hitpoints* hp = iterator->get<Hitpoints>();
+                if (hp->health == hp->healthMax) 
+                    hp->health = 1;
+                hp->health += 2;
+                if (hp->health > hp->healthMax) 
+                    hp->health = hp->healthMax;
+                entities::remove(this);
+                break;
+            }
+        }
+    }
+}
+
+void HealthDrop::render() {
+    activeSprite->render(align, camera::x, camera::y, 2);
+}
+
+HealthDrop::~HealthDrop() {}
