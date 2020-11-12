d59f784c5b40a7e0ed6d1e8949d00b438a7e86c0
Pascal King <pmkgbwis@gmail.com>
2020-11-12 04:52:13 -0600

    completed logic for a particle source
     - particle source manages particle lifetimes
     - also manages particle positions
     - also renders particles
     - higher level particle management to be handled via particle manager

--- engine/headers/particle.h
+++ engine/headers/particle.h
@@ -8,14 +8,20 @@ private:
         Alignment align;
         int lifetime;
     };
-    int size;
-    Particle* particles;
+    Alignment* align = nullptr;
+    int size, loops, z;
+    Particle* particles = nullptr;
     Sprite* sprite;
-    
+    void (*behavior)(Vector2*, int t);
+    int (*lifetime)();
+    void init();
 public:
-    ParticleSource(int size, Sprite* sprite, int (*lifetime)());
-    ParticleSource(int size, Sprite* sprite, int (*lifetime)(), int loops);
+    ParticleSource(int size, int z, Sprite* sprite, int (*lifetime)(), void (*behavior)(Vector2*, int t));
+    ParticleSource(int size, int z, Sprite* sprite, int (*lifetime)(), void (*behavior)(Vector2*, int t), int loops);
     void update();
     void render();
+    void bind(Alignment* align);
+    void start();
+    void stop();
     ~ParticleSource();
 };
--- engine/src/utils/particle.cpp
+++ engine/src/utils/particle.cpp
@@ -1,21 +1,83 @@
 #include "particle.h"
 
-ParticleSource::ParticleSource(int size, Sprite* sprite, int (*lifetime)()) {
-
+ParticleSource::ParticleSource(int size, int z, Sprite* sprite, int (*lifetime)(), void (*behavior)(Vector2*, int t)) {
+    this->size = size;
+    this->loops = 0;
+    this->sprite = sprite;
+    this->lifetime = lifetime;
+    this->behavior = behavior;
+    this->z = z;
 }
 
-ParticleSource::ParticleSource(int size, Sprite* sprite, int (*lifetime)(), int loops) {
-
+ParticleSource::ParticleSource(int size, int z, Sprite* sprite, int (*lifetime)(), void (*behavior)(Vector2*, int t), int loops) {
+    this->size = size;
+    this->loops = loops;
+    this->sprite = sprite;
+    this->lifetime = lifetime;
+    this->behavior = behavior;
+    this->z = z;
 }
 
 void ParticleSource::update() {
+    bool dead = true;
+    for (int i = 0; i < size; i++) {
+        if (particles[i].lifetime) {
+            behavior(&particles[i].align.pos, particles[i].lifetime);
+            dead = false;
+        }
+    }
 
+
+    if (dead) {
+        if (loops > 0) {
+            init();
+            loops--;
+        } 
+        else
+            stop();
+        
+    }
 }
 
 void ParticleSource::render() {
+    for (int i = 0; i < size; i++) {
+        if (particles[i].lifetime)
+            sprite->render(&particles[i].align, z);
+    }
+}
 
+void ParticleSource::bind(Alignment* align) {
+    this->align = align;
+}
+
+void ParticleSource::start() {
+    init();
+    //add to particle manager
+}
+
+void ParticleSource::stop() {
+    //remove from particle manager and free
+}
+
+void ParticleSource::init() {
+    if (particles != nullptr)
+        delete(particles);
+    particles = new Particle[size];
+    if (align == nullptr) {
+        for (int i = 0; i < size; i++) {
+            particles[i].align.pos.x = 0;
+            particles[i].align.pos.y = 0;
+            particles[i].lifetime = lifetime();
+        }
+    } else {
+        for (int i = 0; i < size; i++) {
+            particles[i].align.pos = align->pos;
+            particles[i].lifetime = lifetime();
+        }
+    }
 }
 
 ParticleSource::~ParticleSource() {
-
+    if (particles != nullptr)
+        delete(particles);
 }
