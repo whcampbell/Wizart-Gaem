b78ba71e89f60cbfdb0a1cedfa3d9895dacaf25c
Pascal King <pmkgbwis@gmail.com>
2020-11-22 18:41:55 -0600

    Added screenshake
     - added screenshake() function to camera namespace
     - allows programmer to specify an intensity and duration to shake the screen for
     - projectile and slash attacks now use screenshake

--- game/src/entities/slash.cpp
+++ game/src/entities/slash.cpp
@@ -55,6 +55,10 @@
                 source->start();
                 // end damage number particle
 
+                //do screenshake
+                camera::screenshake(2, 9);
+                //end screenshake
+
                 if (hp->health <= 0)
                     entities::remove(iterator);
                 }
