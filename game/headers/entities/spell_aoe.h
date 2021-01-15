404018fd43c65293e70c599951cc9aca30c7a5fc
Will <Campbell>
2021-01-14 20:59:26 -0600

    Refactor to slots and elements  - Tried my best to avoid walls of switch statements  - helper methods take in spell data in both player and the spell itself  - still just basic structure, needs fleshing out

--- game/headers/entities/spell_aoe.h
+++ game/headers/entities/spell_aoe.h
@@ -6,6 +6,7 @@
 class Spell_AOE : public Entity {
     private:
         Sprite* activeSprite;
+        void cast(int damage, int knock, int dbuff, Entity* iterator);
     public:
         Spell_AOE();
         void update();
--- game/src/entities/spell_aoe.cpp
+++ game/src/entities/spell_aoe.cpp
@@ -34,65 +34,90 @@ Spell_AOE::Spell_AOE() {
 }
 
 void Spell_AOE::update() {
-    float centerx = align->pos.x;
-    float centery = align->pos.y + 32;
+
     EntityList* hit = get<EntityList>();
+    Element* elm = get<Element>();
+    int tag = elm->element;
     Lifetime* life = get<Lifetime>();
     if (life->ticks > 50)
     for (auto iterator : *entities::all()) {
        
         if (!(hit->contains(iterator)) && hitbox::collision(iterator->hitbox("hurtbox"), hitbox("hitbox"))) {
-            
-            if (iterator->has<Physics>()) {
-                float dx = iterator->pos()->pos.x - centerx;
-                float dy = iterator->pos()->pos.y - centery;
-
-                float mag = dx * dx + dy * dy;
-                int shove = 0;
-                if (mag < 50) 
-                    shove = 4;
-                else if (mag < 100)
-                    shove = 3;
-                else
-                    shove = 2;
-                
-                Physics* physics = iterator->get<Physics>();
-                physics->physicsActive = true;
-                physics->velocity.x = shove * cos(dx, dy);
-                physics->velocity.y = shove * sin(dx, dy);
-                physics->acceleration.x = -.08 * cos(dx, dy);
-                physics->acceleration.y = -.08 * sin(dx, dy);
+            switch (tag) {
+                case 1: // fire
+                    cast(2, 4, 1, iterator);
+                    break;
+                case 2: // moist
+                    cast(1, 5, 2, iterator);
+                    break;
+                case 3: // froze
+                    cast(2, 2, 3, iterator);
+                    break;
+                case 4: 
+                    // other elements should have different constants
+                    break;
             }
 
-            if (iterator->has<BuffTimers>()) {
-                BuffTimers* buff = iterator->get<BuffTimers>();
-                buff->onFire = 240;
-            }
-
-            if (iterator->has<Hitpoints>()) {
-                hit->list.push_back(iterator);
-                Hitpoints* hp = iterator->get<Hitpoints>();
-                hp->health = hp->health - 2;
-
-                // damage number particle
-                damagenumber(2, iterator->pos()->pos);
-
-                // hitpause for target
-                iterator->pause(6);
-
-                // remove target if dead
-                if (hp->health <= 0) 
-                    entities::remove(iterator);
-            }
         }
     }
 
-    
     life->ticks--;
     if (!life->ticks) 
         entities::remove(this);
     if (life->ticks == 71) {
         camera::screenshake(5, 20);
+    }
+}
+
+void Spell_AOE::cast(int damage, int knock, int dbuff, Entity* iterator) {
+    EntityList* hit = get<EntityList>();
+    float centerx = align->pos.x;
+    float centery = align->pos.y + 32;
+    if (iterator->has<Physics>()) {
+        float dx = iterator->pos()->pos.x - centerx;
+        float dy = iterator->pos()->pos.y - centery;
+
+        float mag = dx * dx + dy * dy;
+        int shove = 0;
+        if (mag < 75) 
+            shove = knock;
+        else if (mag < 150)
+            shove = knock - 1;
+        else
+            shove = knock - 2;
+        
+        Physics* physics = iterator->get<Physics>();
+        physics->physicsActive = true;
+        physics->velocity.x = shove * cos(dx, dy);
+        physics->velocity.y = shove * sin(dx, dy);
+        physics->acceleration.x = -.08 * cos(dx, dy);
+        physics->acceleration.y = -.08 * sin(dx, dy);
+    }
+
+    if (iterator->has<BuffTimers>()) {
+        BuffTimers* buff = iterator->get<BuffTimers>();
+        if (dbuff == 1) 
+            buff->onFire = 240;
+        if (dbuff == 2) 
+            buff->moist = 600;
+        if (dbuff == 3)
+            buff->frozen = 300;
+    }
+
+    if (iterator->has<Hitpoints>()) {
+        hit->list.push_back(iterator);
+        Hitpoints* hp = iterator->get<Hitpoints>();
+        hp->health = hp->health - damage;
+
+        // damage number particle
+        damagenumber(damage, iterator->pos()->pos);
+
+        // hitpause for target
+        iterator->pause(6);
+
+        // remove target if dead
+        if (hp->health <= 0) 
+            entities::remove(iterator);
     }
 }
 
